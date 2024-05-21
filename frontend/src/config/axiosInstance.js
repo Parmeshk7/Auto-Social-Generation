cde671adaa978af1c50613033cfffa2e837feda7
Parmesh Kumar <92393645+Parmeshk7@users.noreply.github.com>
2024-05-21 23:40:56 +0530

    Connected to production backend server

--- frontend/src/config/axiosInstance.js
+++ frontend/src/config/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8081"
+    baseURL: "http://auto-social-generation-production.up.railway.app"
 })
 
 export default axiosInstance;
